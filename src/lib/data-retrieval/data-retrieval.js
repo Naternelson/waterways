8c1d9962e82e7d81a5b1299afe1a4e99a57c28b5
Nate <7nater.nelson@gmail.com>
2021-12-08 14:47:32 -0700

    basic request made

---
+++ src/lib/data-retrieval/data-retrieval.js
@@ -0,0 +1,37 @@
+const _ = require("lodash")
+const axios = require("axios")
+
+
+
+module.exports = async function entry(params){
+    // checkParams(params)
+    return await request(params)
+}
+
+async function request(params){
+    const BASE_URL = "http://waterservices.usgs.gov/nwis/iv/"
+    try{
+        const res = await axios.get(BASE_URL,{params})
+        return res
+    } catch(err){
+        return err
+    ==
+    
+}
+
+
+function checkParams(params){
+    const any = (...els) => els.some(el => el)
+    const errMessage =  "Params must contain either a Valid Name, Code or Lat/Lon"
+    if(!params) throw new Error(errMessage) 
+    const {lat, lon, name, code} = params 
+    if(!any(lat, lon, name, code)) throw new Error(errMessage) 
+    if(!name && !code){
+        if(!any(lat, lon)) throw new Error(errMessage) 
+    }
+    return params 
+}
+
+function retrieveByName(name){
+    name = _.toUpper(_.trim(name))
+}
--- src/lib/data-retrieval/data-retrieval.test.js
+++ src/lib/data-retrieval/data-retrieval.test.js
@@ -1,19 +1,53 @@
 const dataRetrieval = require("./data-retrieval")
 describe("Data Retrieval should make requests to USGS API and get correct data", ()=> {
+    const params ={
+        name: "STEWART LAKE OUTFLOW NEAR JENSEN, UT",
+        lat: 41.96591667, lon: -111.3981778,
+        code: 10055000
+    }
+    const properParams ={
+        format: 'json',
+        indent: 'on',
+        stateCd: 'ut',
+        parameterCd: '00060,00065',
+        siteType: 'LK',
+        siteStatus: 'all'
+    }
+
+
     describe("The basic response from the request should be valid", ()=>{
-        it.todo("should get 200 response")
-        it.todo("should be in JSON format")
+        it("should get 200 response", () => {
+            const name = params.name
+            expect(dataRetrieval({name}).status).toBe(200)
+        })
+        it.only("should be in JSON format", async ()=>{
+            
+            const res = await dataRetrieval(properParams)
+            console.log({res})
+            expect(res.headers).toBe('application/json')
+        })
     })
     describe("The function should be able to accept proper params", ()=> {
+        const errMessage = "Name, Code or Lat/Lon"
         it("should throw an errow if not presented with proper params", () => {
-            expect(dataRetrieval({random: "property"})).toThrow()
-            expect(dataRetrieval()).toThrow()
+            expect(dataRetrieval({random: "property"})).rejects.toThrow(errMessage)
+            expect(dataRetrieval()).rejects.toThrow(errMessage)
         })
-        it.todo("should accept Name of the Waterway")
-        it.todo("should accept Lat / Lon of the Waterway")
-        it.todo("should accept Code of the Waterway")
+        it("should accept Name of the Waterway", ()=> {
+            const name = params.name
+            expect(dataRetrieval({name})).resolves.not.toThrow(errMessage)
+            expect(dataRetrieval({name: name.toLowerCase()})).resolves.not.toThrow(errMessage)
+        })
+        it("should accept Lat / Lon of the Waterway", ()=>{
+            const {lat, lon} = params
+            expect(dataRetrieval({lat, lon})).resolves.not.toThrow(errMessage)
+        })
+        it("should accept Code of the Waterway", ()=>{
+            const code = params.code
+            expect(dataRetrieval({code})).resolves.not.toThrow(errMessage)
+        })
     })
-    describe("The Function should return nessicary data", () => {
+    describe("The Function should return necessary data", () => {
         it.todo("should have the Name of the Waterway")
         it.todo("should have the Lat / Lon of the Waterway")
         it.todo("should have the Code of the Waterway")
