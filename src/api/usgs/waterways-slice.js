77fd8ab0d035a7ac8a6148c674776d7c10890c90
Nate <7nater.nelson@gmail.com>
2021-12-10 10:54:49 -0700

    Waterways reducer slice

--- src/api/usgs/waterways-slice.js
+++ src/api/usgs/waterways-slice.js
@@ -1,5 +1,85 @@
 import { createSlice } from "@reduxjs/toolkit";
+import {getDistance} from "geolib"
 
-export default createSlice({
-    
+const name = "waterways"
+const initialState = {
+    location: {},
+    loading: true,
+    data: {}
+}
+
+const slice = createSlice({
+    name, 
+    initialState, 
+    reducers: {
+        locationChanged: (store, action) => {
+            const {latitude, longitude, state} = action.payload
+            store.location = {latitude, longitude,state}
+        },
+        waterwayAdded: (store, action) => {
+            const id = action.payload.id 
+            store.data[id] = action.payload 
+        },
+        waterwayDeleted: (store, action) => {
+            const id = action.payload.id 
+            delete store.data[id]
+        },
+        waterwaysAdded: (store, action) => {
+            let {payload} = action 
+            if(Array.isArray(action.payload)){
+                payload = action.payload.reduce((obj, el) => {
+                    obj[el.id] = el 
+                    return obj 
+                },{})
+            }
+
+            store.data = {...store.data, ...payload}
+        },
+        waterwaysDeleted: (store, action) => {
+            let {payload} = action
+            if(Array.isArray(payload)){
+                if(payload[0].id) payload = action.payload.map((el)=>{el.id})
+            } else {
+                payload = Object.keys(action.payload)
+            }
+            payload.forEach((id)=>{
+                delete store.data[id]
+            })
+        },
+        waterwaysLoading: (store) => {
+            store.loading = true 
+        },
+        waterwaysLoaded: (store)=>{
+            store.loading = false 
+        },
+        waterwaysReset: (store)=>{
+            return initialState
+        }
+    }
 })
+
+export default slice.reducer
+export const {locationChanged,waterwayAdded,waterwayDeleted,waterwaysAdded,waterwaysDeleted,waterwaysLoading,waterwaysLoaded,waterwaysReset} = slice.actions
+
+export const waterwaysAsArraySelector = store => {
+    const {data} = store.entries.waterways 
+    return Object.entries(data).map(([key,value]) => value)
+}
+export const sortWaterwaysSelector = (store, closestFirst=true) => {
+    const {location} = store.entities.waterways
+    const arr = waterwaysAsArraySelector(store)
+    if(!location.latitude) return arr  
+    return arr.sort((a, b) => {
+        distanceA = getDistance(location, a.coord)
+        distanceB = getDistance(location, b.coord)
+        if(closestFirst){
+            return a-b
+        } else {
+            return b-a
+        }
+    })
+}
+export const sortByNSWaterwaysSelector = (store, NtoS = true) => {
+
+}
+
