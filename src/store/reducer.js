5c4ab14703e2cb512f93f6eed0e5383f1958893a
Nate <7nater.nelson@gmail.com>
2021-12-16 14:00:27 -0700

    Ui Slice Add to Store

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import entities from './entities-reducer'
+import ui from "./slices/ui-slice"
 
 export default combineReducers({
-    entities
+    entities,
+    ui
 })
